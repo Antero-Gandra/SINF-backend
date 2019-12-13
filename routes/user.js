a9f375b96b6b5aa41d5fb6c124b27be67df57171
up201605769 <up201605769@fe.up.pt>
2019-12-13 22:24:51 +0000

    Supplier login, some debug prints as well

--- routes/user.js
+++ routes/user.js
@@ -9,26 +9,54 @@ router.post("/customer/add", function(req, res, next) {
 
   Customer.find({ tenant, organization, company_uuid })
     .then(response => {
+      console.log(response);
       if (response === null) {
         Customer.create({ tenant, organization, company_uuid: company_uuid })
           .then(response => {
+            console.log("success, created new user");
             console.log(response);
           })
           .catch(error => {
-            console.log("entering catch");
+            console.log("create error");
             res.send(error);
           });
       } else {
-        res.send("User already created ..");
+        console.log("success, found existing user");
+        res.send({ body:"User already created .." });
       }
     })
-    .catch(error => res.send(error));
+    .catch(error => {
+      console.log("find error");
+      res.send(error);
+    });
 });
 
 router.post("/supplier/add", function(req, res, next) {
   let tenant = req.body.tenant;
   let organization = req.body.organization;
-  let company = req.body.company;
+  let company_uuid = req.body.company;
+
+  Supplier.find({ tenant, organization, company_uuid })
+  .then(response => {
+    if (response === null) {
+      Supplier.create({ tenant, organization, company_uuid: company_uuid })
+        .then(response => {
+          console.log("success, created new user");
+          console.log(response);
+        })
+        .catch(error => {
+          console.log("create error");
+          res.send(error);
+        });
+    } else {
+      console.log("success, found existing user");
+      res.send({ body:"User already created .." });
+    }
+  })
+  .catch(error => {
+    console.log("find error");
+    res.send(error);
+  });
 });
 
 module.exports = router;

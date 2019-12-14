78ebdba7e1ff0f99a2f76bb67605dd8db1bff810
up201605769 <up201605769@fe.up.pt>
2019-12-14 23:37:37 +0000

    cant find customer now

--- routes/subscriptions.js
+++ routes/subscriptions.js
@@ -9,12 +9,20 @@ router.get("/get/all/customer", function(req, res, next) {
     let company_uuid = req.query.company;
     
     Customer.find({ tenant, organization, company_uuid }).then(response =>{
-        let userId = response.customer_id;
+        if(response !== null){
+            let userId = response.customer_id;
 
-        Subscription.allCustomer(userId).then(response => {
-            res.send(response);
-        })
+            Subscription.allCustomer(userId).then(response => {
+                res.send(response);
+            })
+        }
+
+        else{
+            res.send({msg:"Customer not found"});
+        }
+
     })
+    .catch(error => console.log(error));
 });
 
 router.get("/get/all/supplier", function(req, res, next) {
@@ -23,12 +31,19 @@ router.get("/get/all/supplier", function(req, res, next) {
     let company_uuid = req.query.company;
     
     Supplier.find({ tenant, organization, company_uuid }).then(response =>{
-        let userId = response.supplier_id;
+        if(response !==  null){
+            let userId = response.supplier_id;
 
-        Subscription.allSupplier(userId).then(response => {
-            res.send(response);
-        })
+            Subscription.allSupplier(userId).then(response => {
+                res.send(response);
+            })
+        }
+
+        else {
+            res.send({msg:"Supplier not found"});
+        }
     })
+    .catch(error => console.log(error));
 });
 
 router.delete("/delete/:id", function(req, res, next) {

1633441aeb685514f1399b8e4581187792b53df5
Ricardo <up201505244@fe.up.pt>
2019-12-14 22:21:23 +0000

    Add brand name to database

--- database/sql/all.sql
+++ database/sql/all.sql
@@ -3,5 +3,5 @@
 \ir triggers.sql
 INSERT INTO customer_user(customer_tenant, customer_organization, customer_company_uuid) VALUES ('123', 'SINFY', '14341234567898761234098767891234');
 INSERT INTO supplier_user (supplier_tenant, supplier_organization, supplier_company_uuid) VALUES ('456','SINFY-B', 'b4022410b4e54417910368d600d54ef2');
-INSERT INTO brand(supplier_id, brand_uuid) VALUES ('2', 'b65c2b831b0a47d2b165c91a3c1db534');
+INSERT INTO brand(supplier_id, brand_uuid, brand_name) VALUES ('2', 'b65c2b831b0a47d2b165c91a3c1db534', 'EXEMPLO');
 INSERT INTO subscription(brand_id, customer_id) VALUES ('1', '1');

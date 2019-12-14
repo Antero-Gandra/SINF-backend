265704bd0e300954dd9eaceb858189a2f1b71d26
up201605769 <up201605769@fe.up.pt>
2019-12-14 23:29:18 +0000

    Company name distinguishable

--- database/sql/all.sql
+++ database/sql/all.sql
@@ -1,7 +1,7 @@
 \ir schema.sql
 \ir views.sql
 \ir triggers.sql
-INSERT INTO customer_user(customer_tenant, customer_organization, customer_company_uuid, customer_company_name) VALUES ('123', 'SINFY', '14341234567898761234098767891234', 'NAME');
-INSERT INTO supplier_user (supplier_tenant, supplier_organization, supplier_company_uuid, supplier_company_name) VALUES ('456','SINFY-B', 'b4022410b4e54417910368d600d54ef2', 'NAME');
+INSERT INTO customer_user(customer_tenant, customer_organization, customer_company_uuid, customer_company_name) VALUES ('123', 'SINFY', '14341234567898761234098767891234', 'CUSTOMER');
+INSERT INTO supplier_user (supplier_tenant, supplier_organization, supplier_company_uuid, supplier_company_name) VALUES ('456','SINFY-B', 'b4022410b4e54417910368d600d54ef2', 'SUPPLIER');
 INSERT INTO brand(supplier_id, brand_uuid, brand_name) VALUES ('2', 'b65c2b831b0a47d2b165c91a3c1db534', 'EXEMPLO');
 INSERT INTO subscription(brand_id, customer_id) VALUES ('1', '1');

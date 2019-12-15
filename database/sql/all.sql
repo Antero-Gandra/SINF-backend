05ed84ef53b799048ba0387b473ee17069ecffb7
up201605769 <up201605769@fe.up.pt>
2019-12-15 19:31:58 +0000

    new brand names

--- database/sql/all.sql
+++ database/sql/all.sql
@@ -3,11 +3,11 @@
 \ir triggers.sql
 INSERT INTO customer_user(customer_tenant, customer_organization, customer_company_uuid, customer_company_name) VALUES ('224977', '224977-0001', '32652d3e-a1ad-4097-8dd6-b78f7503af6d', 'EMPY');
 INSERT INTO supplier_user (supplier_tenant, supplier_organization, supplier_company_uuid, supplier_company_name) VALUES ('227019','227019-0001', '5210709b-9cd7-4d65-960b-c35279faab33', 'EMPBY');
-INSERT INTO brand(supplier_id, brand_uuid, brand_name) VALUES ('2', 'b65c2b831b0a47d2b165c91a3c1db534', 'EXEMPLO');
-INSERT INTO brand(supplier_id, brand_uuid, brand_name) VALUES ('2', 'abe05198abdd4d92800b116ebfc67d73', 'MARCA1');
-INSERT INTO brand(supplier_id, brand_uuid, brand_name) VALUES ('2', '8b998a491a464bb6b70ec63bbe541d73', 'OUTRAMARCA');
-INSERT INTO brand(supplier_id, brand_uuid, brand_name) VALUES ('2', 'c14e22a997f148f5b2d0544e4cf2eeea', 'MARCA6');
-INSERT INTO brand(supplier_id, brand_uuid, brand_name) VALUES ('2', 'b116e709f0a64ab08aae833354fe600b', 'FORMULA1');
+INSERT INTO brand(supplier_id, brand_uuid, brand_name) VALUES ('2', 'b65c2b831b0a47d2b165c91a3c1db534', 'MARCA1');
+INSERT INTO brand(supplier_id, brand_uuid, brand_name) VALUES ('2', 'abe05198abdd4d92800b116ebfc67d73', 'MARCA2');
+INSERT INTO brand(supplier_id, brand_uuid, brand_name) VALUES ('2', '8b998a491a464bb6b70ec63bbe541d73', 'MARCA3');
+INSERT INTO brand(supplier_id, brand_uuid, brand_name) VALUES ('2', 'c14e22a997f148f5b2d0544e4cf2eeea', 'MARCA4');
+INSERT INTO brand(supplier_id, brand_uuid, brand_name) VALUES ('2', 'b116e709f0a64ab08aae833354fe600b', 'MARCA5');
 INSERT INTO subscription(brand_id, customer_id) VALUES ('1', '1');
 INSERT INTO subscription(brand_id, customer_id) VALUES ('2', '1');
 INSERT INTO subscription(brand_id, customer_id) VALUES ('3', '1');

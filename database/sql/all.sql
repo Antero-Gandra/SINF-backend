cf1a0ae706605bd2148c65bcb2249ac94b1c99bf
up201605769 <up201605769@fe.up.pt>
2019-12-15 19:27:55 +0000

    added the rest of the relations

--- database/sql/all.sql
+++ database/sql/all.sql
@@ -9,6 +9,16 @@ INSERT INTO brand(supplier_id, brand_uuid, brand_name) VALUES ('2', '8b998a491a4
 INSERT INTO brand(supplier_id, brand_uuid, brand_name) VALUES ('2', 'c14e22a997f148f5b2d0544e4cf2eeea', 'MARCA6');
 INSERT INTO brand(supplier_id, brand_uuid, brand_name) VALUES ('2', 'b116e709f0a64ab08aae833354fe600b', 'FORMULA1');
 INSERT INTO subscription(brand_id, customer_id) VALUES ('1', '1');
+INSERT INTO subscription(brand_id, customer_id) VALUES ('2', '1');
+INSERT INTO subscription(brand_id, customer_id) VALUES ('3', '1');
+INSERT INTO subscription(brand_id, customer_id) VALUES ('4', '1');
+INSERT INTO subscription(brand_id, customer_id) VALUES ('5', '1');
 INSERT INTO sp_item(subscription_id, supplier_item, customer_item) VALUES ('1', 'ARTIGO','ART');
 INSERT INTO sp_item(subscription_id, supplier_item, customer_item) VALUES ('1', 'EXEMPLO','EX');
 INSERT INTO sp_item(subscription_id, supplier_item, customer_item) VALUES ('1', 'PORTES','PORT'); 
+INSERT INTO sp_item(subscription_id, supplier_item, customer_item) VALUES ('2', 'wgweg','wgRT');
+INSERT INTO sp_item(subscription_id, supplier_item, customer_item) VALUES ('2', 'EXEMeePLO','EcX');
+INSERT INTO sp_item(subscription_id, supplier_item, customer_item) VALUES ('3', 'PORccTES','POvRT'); 
+INSERT INTO sp_item(subscription_id, supplier_item, customer_item) VALUES ('4', 'ARaTIGO','AffRT');
+INSERT INTO sp_item(subscription_id, supplier_item, customer_item) VALUES ('5', 'EXEMcccPLO','EbbbX');
+INSERT INTO sp_item(subscription_id, supplier_item, customer_item) VALUES ('5', 'PORTEczzS','PORzzzT'); 
